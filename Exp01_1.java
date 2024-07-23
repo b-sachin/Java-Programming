0b5aca679349363e3bb8a61949f2b8114306042c
Sachin Bhopi <sachinbhopiofficial@gmail.com>
2024-07-23 11:28:52 +0530

    Experiment Number 1

---
+++ Exp01_1.java
@@ -0,0 +1,101 @@
+import java.util.*;
+class Exp01_1
+{
+	public static void main(String args[])
+	{
+
+		int choice=0;
+		int n;
+		Scanner sc = new Scanner(System.in);
+
+
+
+		while(true)
+		{
+			System.out.println("##### Menu #####");
+			System.out.println("1.Factorial");
+			System.out.println("2.Palindrome");
+			System.out.println("3.Prime");
+			System.out.println("4.Exit");
+			System.out.println("Enter your choice: ");
+
+			choice = sc.nextInt();
+		
+			switch(choice)
+			{
+				case 1:
+					System.out.println("Enter Number: ");
+					n = sc.nextInt();
+					int fact = 1;
+					if(n<0)
+					{
+						System.out.println("Not Possible");
+					}
+					else if(n==0)
+					{
+						System.out.println(fact);
+					}
+					else
+					{
+						for(int i=n;i>0;i--)
+						{
+							fact=fact*i;
+						}
+						System.out.println(fact);
+					}
+					break;
+				
+				case 2:
+					System.out.println("Enter Number to be checked: ");		
+					n =sc.nextInt();
+					int temp=n;
+					int sum=0;
+					int count=0;
+					while(n!=0)
+					{
+						int d = n%10;
+						sum= sum*10 + d;
+						n=n/10;
+					}
+					if(sum==temp)
+					{
+						System.out.println("Palindrome");
+					}
+					else
+					{
+						System.out.println("not Palindrome");	
+					}	
+					break;	
+
+				case 3:
+					System.out.println("Enter number to be checked:");
+					n = sc.nextInt();
+					boolean flag = false;
+					for(int i=2; i<=n/2; i++)
+					{
+						if(n%i==0)
+						{
+							flag = true;
+							break;
+						}
+					}
+					if(flag)
+					{
+						System.out.println("Not Prime");
+					}
+					else
+					{
+						System.out.println("Prime");
+					}
+					break;	
+	
+				case 4:
+					System.exit(0);
+
+				default:
+					System.out.println("Invalid choice");
+					break;							
+			}
+		}
+	}
+}		
